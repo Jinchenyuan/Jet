ae9036cfbb100b3e512804f7090248503e1155f0
jinchenyuan <jinchenyuan@outlook.com>
2020-05-06 01:57:04 +0800

    hello world

--- core/jet_sem.c
+++ core/jet_sem.c
@@ -1,6 +1,31 @@
 #include "jet_sem.h";
 
+jet_int sem_get(jet_int key)
+{
+    jet_int semfd = semget(key, 1, SEM_FLG_CHECK);
+    if (semfd != -1)
+        return semfd;
+    else
+        goto NOT_FOUND;
+NOT_FOUND:
+    return JET_ERROR;
+}
 
+void sem_del(jet_int key)
+{
+    jet_int semfd = sem_get(key);
+    if (semfd != JET_ERROR) {
+        
+    }
+
+}
+
+jet_int sem_create(jet_int key)
+{
+    sem_del(key);
+}
+
+/**************************************************test**************************************/
 void sem_pre_test()
 {
     jet_int semid = semget(SEM_LOG_KEY, 1, 0666|IPC_EXCL|IPC_CREAT);
--- core/jet_sem.h
+++ core/jet_sem.h
@@ -6,11 +6,14 @@
 #include <sys/ipc.h>
 #include "jet_define.h"
 
+#define SEM_FLG_CHECK       0640
+
 #define SEM_LOG_KEY         0x70001
 #define SEM_TCP_KEY         0x70002
 #define SEM_UDP_KEY         0x70003
 #define SEM_HTTP_KEY        0x70004
 
+/******************************************test************************************/
 void sem_pre_test();
 
 #endif
